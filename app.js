227f1f0cc3bdc0461f1c522aac09820ce3a99a99
Kilu691 <kilu691@gmail.com>
2020-10-05 17:34:19 +0200

    Implement basic functionality

--- app.js
+++ app.js
@@ -1,45 +1,75 @@
+/* eslint-disable no-use-before-define */
 const form = document.querySelector('.search-form');
 const artistInput = document.querySelector('input[name="artist"]');
 const titleInput = document.querySelector('input[name="title"]');
-const lyricsContainer = document.querySelector('.lyrics');
+const songsContainer = document.querySelector('.songs');
 const proxy = 'https://cors-anywhere.herokuapp.com/';
 const endpoint = 'http://api.musixmatch.com/ws/1.1/';
 const apikey = 'c62ff897dc4898e90376bd591ca2c32e';
 
+function handleSubmit(e) {
+  e.preventDefault();
+  songsContainer.innerHTML = '';
+  if (artistInput.value && titleInput.value) {
+    fetchSong();
+  } else if (artistInput.value) {
+    fetchArtistSongs();
+  } else if (titleInput.value) {
+    fetchSongsByTitle();
+  } else {
+    handleNoInputError();
+  }
+}
+
 async function fetchSong() {
   const response = await fetch(
-    `${proxy}${endpoint}track.search?q_artist=${artistInput.value}&q_track=${titleInput.value}&s_track_rating=desc&f_has_lyrics&apikey=${apikey}`
+    `${proxy}${endpoint}track.search?q_artist=${artistInput.value}&q_track=${titleInput.value}&s_track_rating=desc&f_has_lyrics=1&apikey=${apikey}`
   );
   const data = await response.json();
-  return data.message.body.track_list[0];
+  const dataArray = data.message.body.track_list;
+  displaySongs(dataArray);
 }
 
 async function fetchArtistSongs() {
   const response = await fetch(
-    `${proxy}${endpoint}track.search?q_artist=${artistInput.value}&s_track_rating=desc&f_has_lyrics&apikey=${apikey}`
+    `${proxy}${endpoint}track.search?q_artist=${artistInput.value}&s_track_rating=desc&page_size=10&apikey=${apikey}`
   );
   const data = await response.json();
-  return data.message.body.track_list[0];
+  const dataArray = data.message.body.track_list;
+  displaySongs(dataArray);
 }
 
 async function fetchSongsByTitle() {
   const response = await fetch(
-    `${proxy}${endpoint}track.search?q_track=${titleInput.value}&s_track_rating=desc&f_has_lyrics&apikey=${apikey}`
+    `${proxy}${endpoint}track.search?q_track=${titleInput.value}&s_track_rating=desc&f_has_lyrics=1&apikey=${apikey}`
   );
   const data = await response.json();
-  return data.message.body.track_list[0];
+  const dataArray = data.message.body.track_list;
+  displaySongs(dataArray);
 }
 
-async function getLyrics() {
-  let response;
-  if (artistInput.value && titleInput.value) {
-    response = await fetchSong();
-  } else if (artistInput.value) {
-    response = await fetchArtistSongs();
-  } else if (titleInput.value) {
-    response = await fetchSongsByTitle();
+function displaySongs(songs) {
+  if (songs.length) {
+    songs.forEach((e) => {
+      const song = document.createElement('div');
+      song.classList.add('song');
+      song.id = e.track.track_id;
+      song.insertAdjacentHTML(
+        'afterbegin',
+        `${e.track.artist_name} ${e.track.track_name} <button>See lyrics</button>`
+      );
+      songsContainer.appendChild(song);
+      song.addEventListener('click', (event) =>
+        displayLyrics(parseInt(event.currentTarget.id))
+      );
+    });
+  } else {
+    handleNoLyricsError();
   }
-  const trackId = response.track.track_id;
+}
+
+async function getLyrics(id) {
+  const trackId = id;
   const data = await fetch(
     `${proxy}${endpoint}track.lyrics.get?track_id=${trackId}&apikey=${apikey}`
   );
@@ -47,10 +77,17 @@ async function getLyrics() {
   return lyrics.message.body.lyrics.lyrics_body;
 }
 
-async function displayLyrics(e) {
-  e.preventDefault();
-  const lyricsText = await getLyrics();
-  lyricsContainer.textContent = `${lyricsText}`;
+function handleNoLyricsError() {
+  prompt('No lyrics found');
+}
+function handleNoInputError() {
+  prompt('No input');
 }
 
-form.addEventListener('submit', displayLyrics);
+async function displayLyrics(id) {
+  const lyricsText = await getLyrics(id);
+  songsContainer.innerHTML = `${lyricsText.replace(/(\r\n|\r|\n)/g, '<br>')}`;
+}
+
+form.addEventListener('submit', handleSubmit);
+// ! work on custom error popups
